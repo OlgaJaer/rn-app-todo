9040157b49e48490ed3f2b21e28bf5f13b5fc849
Olga Jaeger <olgar.mamaeva@rambler.ru>
2020-06-27 18:05:50 +0200

    create context, app refactoring, create reducer

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import { StyleSheet, View, Alert } from 'react-native'
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo'
 
-import { Navbar } from './src/components/Navbar'
-import { MainScreen } from './src/screens/MainScreen'
-import { TodoScreen } from './src/screens/TodoScreen'
-import { THEME } from './src/theme'
+import { MainLayout } from './src/MainLayout'
+import { TodoState } from './src/context/todo/TodoState'
 
 async function loadApplication() {
   await Font.loadAsync({
@@ -17,8 +15,6 @@ async function loadApplication() {
 
 export default function App() {
   const [isReady, setIsReady] = useState(false)
-  const [todoId, setTodoId] = useState(null)
-  const [todos, setTodos] = useState([{ id: '1', title: 'learn German' }])
 
   if (!isReady) {
     return (
@@ -30,101 +26,9 @@ export default function App() {
     )
   }
 
-  const addTodo = (title) => {
-    // const newTodo = {
-    //   id: Date.now().toString(),
-    //   title: title
-    // }
-
-    //setTodos(todos.concat([newTodo]))
-
-    // setTodos((prevTodos) => {
-    //   return [
-    //     ...prevTodos,
-    //     newTodo
-    //   ]
-    // })
-    setTodos((prev) => [
-      ...prev,
-      {
-        id: Date.now().toString(),
-        title,
-      },
-    ])
-  }
-
-  const removeTodo = (id) => {
-    const todo = todos.find((t) => t.id === id)
-    Alert.alert(
-      'Delete item',
-      `Are you sure you want to delete "${todo.title}"?`,
-      [
-        {
-          text: 'Cancel',
-          style: 'cancel',
-        },
-        {
-          text: 'Delete',
-          style: 'destructive',
-          onPress: () => {
-            setTodoId(null)
-            setTodos((prev) => prev.filter((todo) => todo.id !== id))
-          },
-        },
-      ],
-      { cancelable: false }
-    )
-  }
-
-  const updateTodo = (id, title) => {
-    setTodos((old) =>
-      old.map((todo) => {
-        if (todo.id === id) {
-          todo.title = title
-        }
-        return todo
-      })
-    )
-  }
-
-  let content = (
-    <MainScreen
-      todos={todos}
-      addTodo={addTodo}
-      removeTodo={removeTodo}
-      openTodo={setTodoId}
-    />
-  )
-
-  if (todoId) {
-    const selectedTodo = todos.find((todo) => todo.id === todoId)
-    content = (
-      <TodoScreen
-        onRemove={removeTodo}
-        goBack={() => setTodoId(null)}
-        todo={selectedTodo}
-        onSave={updateTodo}
-      />
-    )
-  }
   return (
-    <View>
-      <Navbar title='Todo App' />
-      <View style={styles.container}>
-        {content}
-        {/* <View>
-          {todos.map((todo) => (
-            <Todo todo={todo} key={todo.id}></Todo>
-          ))}
-        </View> */}
-      </View>
-    </View>
+    <TodoState>
+      <MainLayout />
+    </TodoState>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: THEME.PADDING_HORIZONTAL,
-    paddingVertical: 20,
-  },
-})
